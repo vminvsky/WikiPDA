ccdca1e1cb49e799d09a2c9c24547e714dd4d769
vminvsky <46860506+vminvsky@users.noreply.github.com>
2022-03-23 16:40:19 +0100

    Update settings.py

--- WikiPDA-Lib/wikipda/settings.py
+++ WikiPDA-Lib/wikipda/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 # These are the paths to the downloaded resources from the drive folder. You can change these
 # as you wish.
-DATA_DIR = str(Path.home()) + '/wikipda_data/'
+DATA_DIR = "/scratch/venia" + '/wikipda_data/'
 LDA_MODEL_PATH = DATA_DIR + 'LDA_models/'
 CLASSIFIERS_PATH = DATA_DIR + 'classifiers/'
 RESOURCE_PATH = DATA_DIR + 'lang/'
